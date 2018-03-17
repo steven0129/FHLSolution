8e41128cb1e476b8f92202b418c447320f2b31db
steven0129 <h3584935@gmail.com>
2018-03-18 01:21:07 +0800

    TODO: 尚未成功編譯

---
+++ NUIVUE/src/components/NuiToolBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="fhlToolBar">
+        <div id="help"></div>
+        <div id="windowControl"></div>
+        <div id="bookSelect"></div>
+        <div id="searchTool" class="smallText"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
---
+++ NUIVUE/src/components/NuiVersionSelect.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="versionSelect"></div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
